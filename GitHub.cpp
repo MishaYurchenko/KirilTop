23f48b14c25cfa5989470a4c6842285d3380b1dc
MishaYurchenko <misaurcenko009@gmail.com>
2022-07-09 19:13:14 +0300

    'new1'

---
+++ GitHub.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iostream>
+#include <windows.h>
+#include <string.h>
+
+using namespace std;
+int main()
+{
+
+	system("cd C:\\Users\\student\\source\\repos\\zxc");
+    system("git init");
+	system("mk");
+	system("git add GitHub.cpp");
+	system("git commit");
+
+
+
+}
